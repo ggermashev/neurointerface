d75a6aa97fa66af4f133412488392d3a1c20d717
ggermashev <ggermashev@gmail.com>
2022-12-10 16:23:53 +0300

    measures

---
+++ main.py
@@ -0,0 +1,31 @@
+import time
+
+import serial
+import matplotlib.pyplot as plt
+import numpy as np
+
+def serial_port():
+    ser = serial.Serial('COM4', timeout=1)
+    sum = 0
+    n = 1
+    count = 0
+    while (1):
+        arr = []
+        count += 1
+        sum = 0
+        for i in range(n):
+            res = ser.read(50000)
+            res = list(res)
+            print(res)
+            furie_arr = np.fft.fft(res)
+            arr = np.hstack([arr, np.abs(furie_arr)])
+            print(len(furie_arr))
+            sum += len(furie_arr)
+        freq = np.fft.fftfreq(len(arr), n/sum)
+        plt.plot(freq, arr)
+        plt.savefig(f"./mixed/{count}.png")
+        plt.show()
+    ser.close()
+
+
+serial_port()
